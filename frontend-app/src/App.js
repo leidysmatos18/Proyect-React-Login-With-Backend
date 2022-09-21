dba89578edd2865e10503981588f43af35f1fae1
xocialex <hecate@hecate.com>
2022-09-21 15:27:24 -0400

    aqui ando

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Product from './pages/product';
 import Services from './pages/services';
 import AboutUs from './pages/aboutUs';
